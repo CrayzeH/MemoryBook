d95c48333afba0afa3426c6d819b8c398dae5f98
CrayzeH <vvsdfvvs@gmail.com>
2025-02-15 18:04:24 +0500

    Начало

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+GLOBAL_AUTHORIZE = 0
+
+@app.route('/', methods=["GET", "POST"])
+def main():
+    return render_template("main.html")
+
+if __name__ == '__main__':
+    app.run()
