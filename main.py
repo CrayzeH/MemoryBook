30e1100da8eeaaf8498ced48ea5fd71f3128671d
CrayzeH <vvsdfvvs@gmail.com>
2025-02-16 14:37:59 +0500

    реализована функция авторизации. Форма авторизации ещё не закончена.

--- main.py
+++ main.py
@@ -8,31 +8,38 @@ GLOBAL_AUTHORIZE = 0
 def main():
     if request.method == "POST":
         print(request.form["search-data"])
-    return render_template("main.html")
+    return render_template("main.html", is_authorized=GLOBAL_AUTHORIZE)
 
 @app.route('/hero-search', methods=["GET", "POST"])
 def hero_search():
     if request.method == "POST":
         print(request.form['input-name'])
-    return render_template("hero-search.html")
+    return render_template("hero-search.html", is_authorized=GLOBAL_AUTHORIZE)
 
 @app.route('/contacts', methods=["GET", "POST"])
 def contacts():
     if request.method == "POST":
         print(request.form)
-    return render_template("contacts.html")
+    return render_template("contacts.html", is_authorized=GLOBAL_AUTHORIZE)
 
 @app.route('/full_search', methods=["GET", "POST"])
 def full_search():
     if request.method == "POST":
         print(request.form)
-    return render_template("full-search.html")
+    return render_template("full-search.html", is_authorized=GLOBAL_AUTHORIZE)
 
 @app.route('/send_data', methods=["GET", "POST"])
 def send_data():
     if request.method == "POST":
         print(request.form)
-    return render_template("send-data.html")
+    return render_template("send-data.html", is_authorized=GLOBAL_AUTHORIZE)
+
+@app.route('/authorization', methods=["GET", "POST"])
+def authorize():
+    global GLOBAL_AUTHORIZE
+    if request.method == "POST":
+        GLOBAL_AUTHORIZE = 1
+    return render_template("authorization.html", is_authorized=GLOBAL_AUTHORIZE)
 
 if __name__ == '__main__':
     app.run()
